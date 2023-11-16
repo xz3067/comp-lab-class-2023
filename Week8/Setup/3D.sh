d37002be585890b676eb98118ef7ab0e62d6523b
xz3067 <69778932+xz3067@users.noreply.github.com>
2023-11-17 04:07:11 +0800

    Update 3D.sh

--- Week8/Setup/3D.sh
+++ Week8/Setup/3D.sh
@@ -6,7 +6,7 @@ LOG_DIR="../Data/3DLog"
 TRAJ_DIR="../Data/3DTrajectories"
 
 # Run simulations for densities from 0.5 to 1.1 with a step of 0.1
-for density in $(seq 1.5 0.1 1.5)
+for density in $(seq 0.8 0.1 1.5)
 do
 
     LOGFILE="$LOG_DIR/simulation_3D_density_$density.log"
